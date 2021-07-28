9b5d66d430dc2baf9b7db75e1ebce9dd4481934d
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-07-28 21:30:40 +0300

    impl task 7

--- src/main/java/com/ss/ita/kata/implementation/vmuravskyi/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vmuravskyi/EightImpl.java
@@ -56,10 +56,18 @@ public class EightImpl implements Eight {
 
     @Override
     public boolean amIWilson(double n) {
-        return false;
+        return (factorial(n-1) + 1) % (n*n) == 0;
+    }
+    public static double factorial(double number) {
+        if (number <= 1) {
+            return 1;
+        } else {
+            return number * factorial(number - 1);
+        }
     }
 
-    @Override
+
+        @Override
     public double twoDecimalPlaces(double number) {
         return 0;
     }
