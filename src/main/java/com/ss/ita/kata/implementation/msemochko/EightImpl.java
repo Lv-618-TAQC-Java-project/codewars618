8d9349b042741fa98690d41e8b6956c7d8ca478d
Max <max.mageriv@ukr.net>
2021-07-28 19:35:22 +0300

    divisibleBy task done

--- src/main/java/com/ss/ita/kata/implementation/msemochko/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/msemochko/EightImpl.java
@@ -79,6 +79,24 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        if (divider == 0) {
+            return null;
+        }
+        int j = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % divider == 0) {
+                j++;
+            }
+        }
+
+        int[] tempArr = new int[j];
+        int counter = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % divider == 0) {
+                tempArr[counter] = numbers[i];
+                counter++;
+            }
+        }
+        return tempArr;
     }
 }
