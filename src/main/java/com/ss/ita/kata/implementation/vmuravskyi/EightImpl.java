6b90c0013ff0b7d147cefd3d797aaecc4073a055
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-07-29 01:06:00 +0300

    impl feature8new

--- src/main/java/com/ss/ita/kata/implementation/vmuravskyi/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vmuravskyi/EightImpl.java
@@ -2,7 +2,6 @@ package com.ss.ita.kata.implementation.vmuravskyi;
 
 import com.ss.ita.kata.Eight;
 
-
 public class EightImpl implements Eight {
     @Override
     public int liters(double time) {
