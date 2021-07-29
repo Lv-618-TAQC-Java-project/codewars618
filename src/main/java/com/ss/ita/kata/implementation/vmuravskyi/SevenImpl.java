03d07f9cd4e2dc25e8fe783685a4d0a8c8c6df4c
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-07-29 14:12:37 +0300

    impl task newAvg

--- src/main/java/com/ss/ita/kata/implementation/vmuravskyi/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vmuravskyi/SevenImpl.java
@@ -5,7 +5,13 @@ import com.ss.ita.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sumaVsixDonations = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumaVsixDonations += arr[i];
+        }
+        double result = Math.round((navg * (arr.length + 1)) - sumaVsixDonations);
+        if (result <= 0) throw new IllegalArgumentException();
+        return (long) result;
     }
 
     @Override
