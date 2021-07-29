f806b4e657248baf68718099e8ad802053caa505
Max <max.mageriv@ukr.net>
2021-07-29 18:33:14 +0300

    optimized the code

--- src/main/java/com/ss/ita/kata/implementation/msemochko/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/msemochko/SevenImpl.java
@@ -2,17 +2,20 @@ package com.ss.ita.kata.implementation.msemochko;
 
 import com.ss.ita.kata.Seven;
 
-import java.math.BigInteger;
 
 public class SevenImpl implements Seven {
-
+    public static void main(String[] args) {
+        SevenImpl seven = new SevenImpl();
+        double[] arr = new double[]{14, 30, 5, 7, 9, 11, 15};
+        seven.newAvg(arr, 30);
+    }
     @Override
     public long newAvg(double[] arr, double navg) {
         double sumOfNumbers = 0;
         for (int i = 0; i < arr.length; i++) {
             sumOfNumbers += arr[i];
         }
-        int unknown = BigInteger.valueOf((long) ((navg * (arr.length+1)) - sumOfNumbers)).intValue();
+        int unknown = ((int) ((navg * (arr.length+1)) - sumOfNumbers));
         return unknown;
     }
 
