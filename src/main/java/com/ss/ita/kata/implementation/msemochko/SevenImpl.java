65c64a1ac31a8daf51330eaeb729b852508f2652
Max <max.mageriv@ukr.net>
2021-07-29 18:30:40 +0300

    newAvg task done

--- src/main/java/com/ss/ita/kata/implementation/msemochko/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/msemochko/SevenImpl.java
@@ -2,10 +2,18 @@ package com.ss.ita.kata.implementation.msemochko;
 
 import com.ss.ita.kata.Seven;
 
+import java.math.BigInteger;
+
 public class SevenImpl implements Seven {
+
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sumOfNumbers = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumOfNumbers += arr[i];
+        }
+        int unknown = BigInteger.valueOf((long) ((navg * (arr.length+1)) - sumOfNumbers)).intValue();
+        return unknown;
     }
 
     @Override
