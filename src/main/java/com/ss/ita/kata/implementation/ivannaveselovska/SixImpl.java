58273c576708a93cf2f7d71993d7d59de23acdf7
Ivanna <veselka19012001@gmail.com>
2021-07-30 19:03:16 +0300

    #27 balance function fixed

--- src/main/java/com/ss/ita/kata/implementation/ivannaveselovska/SixImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/ivannaveselovska/SixImpl.java
@@ -20,7 +20,6 @@ public class SixImpl implements Six {
     @Override
     public String balance(String book) {
         String[] lines = getCleanStringArray(book);
-int j = 1;
         double balance = Double.parseDouble(lines[0]);
         double currentBalance = balance;
         StringBuilder sb = new StringBuilder();
@@ -69,8 +68,8 @@ int j = 1;
         List<String> list = new ArrayList<>();
         for (String x : lines) {
             if (x.isEmpty() == false) {
-//                String s = x.replaceAll("(\\W)", "");
-                list.add(x);
+                String s = x.replaceAll("[^\\da-zA-Z .]", "");
+                list.add(s);
             }
         }
         lines = new String[list.size()];
