8ffb47182a5ac4b51fd46ac19121df9537702b7f
mac <mac@MacBook-Air-mac.local>
2021-07-30 09:27:55 +0300

    first task

---
+++ src/main/java/com/ss/ita/kata/implementation/kubrub/EightImpl.java
@@ -0,0 +1,51 @@
+package com.ss.ita.kata.implementation.kubrub;
+
+import com.ss.ita.kata.Eight;
+
+public class EightImpl implements Eight {
+    @Override
+    public int liters(double time) {
+        double litr = Math.floor(time * 0.5);
+        return (int) litr;
+    }
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
