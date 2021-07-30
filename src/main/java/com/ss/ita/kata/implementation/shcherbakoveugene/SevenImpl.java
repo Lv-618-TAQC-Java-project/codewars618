04300817db42fd55117bcaeb0915e2e322caf5cd
Eugene <ievgeniishcherbakov@gmail.com>
2021-07-30 14:02:40 +0300

    feature/#147

--- src/main/java/com/ss/ita/kata/implementation/shcherbakoveugene/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/shcherbakoveugene/SevenImpl.java
@@ -2,6 +2,9 @@ package com.ss.ita.kata.implementation.shcherbakoveugene;
 
 import com.ss.ita.kata.Seven;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
@@ -10,7 +13,21 @@ public class SevenImpl implements Seven {
 
     @Override
     public String seriesSum(int n) {
-        return null;
+        if (n <= 0) {
+            return "0.00";
+        }
+        int divider = 1;
+        double dResult = 1;
+
+        for (int i = 0; i < n-1; i++) {
+            divider += 3;
+            dResult += (1d/divider);
+        }
+
+        BigDecimal result = new BigDecimal(dResult);
+        result = result.setScale(2, RoundingMode.HALF_UP);
+
+        return result.toString();
     }
 
     @Override
