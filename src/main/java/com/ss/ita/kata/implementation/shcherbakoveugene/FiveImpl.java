664eda3b5eb6babe69d5ee457f4b64b54e11ceb0
Eugene <ievgeniishcherbakov@gmail.com>
2021-07-31 20:34:10 +0300

    feature/#177

--- src/main/java/com/ss/ita/kata/implementation/shcherbakoveugene/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/shcherbakoveugene/FiveImpl.java
@@ -3,11 +3,40 @@ package com.ss.ita.kata.implementation.shcherbakoveugene;
 import com.ss.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FiveImpl implements Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+        int count = 1;
+        List<Integer> list = new ArrayList<>();
+
+        if (v.length == 1) {
+            return 1;
+        }
+
+        for (int i = 0; i < v.length; i++) {
+            if (i == v.length - 1) {
+                list.add(count);
+                break;
+            }
+            if (v[i] < v[i + 1]) {
+                list.add(count);
+                count = 1;
+                list.add(count);
+            } else if (v[i] >= v[i + 1]) {
+                count++;
+                if (i != 0) {
+                    if (v[i] > v[i - 1]) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return Collections.max(list);
     }
 
     @Override
