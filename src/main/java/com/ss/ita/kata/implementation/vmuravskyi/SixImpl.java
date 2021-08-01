7f75b8aa2be12991ab261aade2270ea35cf4ef15
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-08-01 17:30:01 +0300

    simplified code v.1.0

--- src/main/java/com/ss/ita/kata/implementation/vmuravskyi/SixImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vmuravskyi/SixImpl.java
@@ -20,49 +20,38 @@ public class SixImpl implements Six {
     public String balance(String book) {
         String[] bookArray = book.split("\n");
 
-        StringBuffer sb = new StringBuffer(bookArray[0]);
-
         for (int i = 0; i < bookArray.length; i++) {
-            bookArray[i] = bookArray[i].replaceAll("\"", "");
+            bookArray[i] = bookArray[i].replace("\"", "");
         }
 
+        double totalBalance = Double.parseDouble(bookArray[0]);
+        bookArray[0] = "Original Balance: ".concat(bookArray[0]);
 
-        Double totalBalance = Double.valueOf(String.valueOf(bookArray[0]));
-        bookArray[0] = "Original_Balance: ".concat(bookArray[0]);
-
-        //GET SPENDING IN ARRAY
-//        double[] spending = new double[bookArray.length];
-//        for (int i = 0; i < bookArray.length; i++) {
-//            spending[i] =
-//        }
         double totalExpense = 0.00;
         double[] spending = new double[bookArray.length];
         for (int i = 1; i < bookArray.length; i++) {
             String[] temp = bookArray[i].split(" ");
-            spending[i] = Double.valueOf(temp[2]);
+            spending[i] = Double.parseDouble(temp[temp.length-1]);
             totalExpense += spending[i];
         }
-        totalExpense = Math.round(totalExpense * 100);
-        totalExpense = totalExpense / 100;
-        double averageExpense = totalExpense / (bookArray.length - 1);
-        averageExpense = Math.round(averageExpense*100);
-        averageExpense = averageExpense/100;
+
+        totalExpense = Double.parseDouble(String.format("%.2f",totalExpense));
+        double averageExpense = Double.parseDouble(String.format("%.2f",totalExpense / (bookArray.length - 1)));
 
         String result = "".concat(bookArray[0] + "\n");
         String add = "Balance ";
-        double restBalance = 0.00;
-        double spent = 0.00;
-        double averageExpance = 0.00;
+        double restBalance;
+
         for (int i = 1; i < bookArray.length; i++) {
-//            restBalance = totalBalance-spending[i];
             restBalance = Math.round((totalBalance - spending[i]) * 100);
             restBalance /= 100;
             totalBalance = totalBalance - spending[i];
             result = result.concat(bookArray[i] + " " + add + restBalance + "\n");
         }
-        String TotalExp = "Total expense: " + totalExpense;
-        String AverExp = "\nAverage expence: " + averageExpense;
-        return result.concat(TotalExp).concat(AverExp);
+
+        String totalExp = "Total expense: " + totalExpense;
+        String averExp = "\nAverage expense: " + averageExpense;
+        return result.concat(totalExp).concat(averExp);
     }
 
 
