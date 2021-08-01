92a0915b6aa9149197f27905d74691606ca9789c
Max <max.mageriv@ukr.net>
2021-08-01 14:23:47 +0300

    artificialRain task done

--- src/main/java/com/ss/ita/kata/implementation/msemochko/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/msemochko/FiveImpl.java
@@ -3,13 +3,40 @@ package com.ss.ita.kata.implementation.msemochko;
 import com.ss.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class FiveImpl implements Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
-    }
+        if (v == null) {
+            return 0;
+        } else if (v.length == 1) {
+            return 1;
+        }
+        int maxNumber = 0;
+        int biggerThan;
 
+        for (int i = 0; i < v.length; i++) {
+            biggerThan = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (v[j]>=v[j+1]){
+                    biggerThan++;
+                }
+            }
+            for (int j = i; j < v.length-1;j++){
+                if (v[j+1]>=v[j]){
+                    biggerThan++;
+                }
+            }
+            if (maxNumber < biggerThan) {
+                maxNumber = biggerThan;
+            }
+        }
+        return maxNumber;
+    }
     @Override
     public long[] gap(int g, long m, long n) {
         return new long[0];
