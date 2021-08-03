74b88ea4af4c462c8cf5a3e2ddff03aee7140998
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-08-03 11:50:14 +0300

    update

--- src/main/java/com/ss/ita/kata/implementation/vmuravskyi/SixImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vmuravskyi/SixImpl.java
@@ -35,8 +35,8 @@ public class SixImpl implements Six {
             totalExpense += spending[i];
         }
 
-        totalExpense = Double.parseDouble(String.format("%.2f",totalExpense));
-        double averageExpense = Double.parseDouble(String.format("%.2f",totalExpense / (bookArray.length - 1)));
+        totalExpense = Double.parseDouble(String.format("%3.2f",totalExpense));
+        double averageExpense = Double.parseDouble(String.format("%3.2f",totalExpense / (bookArray.length - 1)));
 
         String result = "".concat(bookArray[0] + "\n");
         String add = "Balance ";
