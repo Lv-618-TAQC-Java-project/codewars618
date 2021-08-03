a814b2f49f13bc0ffd9eca0b9b47067863342cf0
Yura Nesterovych <yranest2001@gmail.com>
2021-08-03 16:29:30 +0300

    Feature/#238

--- src/main/java/com/ss/ita/kata/implementation/yranest/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/yranest/EightImpl.java
@@ -26,14 +26,13 @@ public class EightImpl implements Eight {
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
         {
-            int[] array = new int[2];
-            for(int i=0; i<input.length;i++){
-                if(input[i]>0)
-                    array[0]+=1;
-                else
-                    array[1]+=input[i];
+            if (input == null || input.length == 0) return new int[] {};
+            int count = 0,sum = 0;
+            for (int i : input) {
+                if (i > 0) count ++;
+                if (i < 0) sum += i;
             }
-            return array;
+            return new int[] {count,sum};
         }
     }
 
