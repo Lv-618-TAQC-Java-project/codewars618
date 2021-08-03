fc5a9a157164c2674192ce18fa1c76f9abb6039e
Yura Nesterovych <yranest2001@gmail.com>
2021-08-03 17:01:41 +0300

    Feature/#248

--- src/main/java/com/ss/ita/kata/implementation/yranest/SevenImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/yranest/SevenImpl.java
@@ -5,7 +5,11 @@ import com.ss.ita.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) sum += arr[i];
+        long ans = (long) Math.ceil((arr.length + 1) * navg - sum);
+        if (ans < 0) throw new IllegalArgumentException();
+        return ans;
     }
 
     @Override
