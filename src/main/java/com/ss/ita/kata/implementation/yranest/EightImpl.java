aca07222bf9cbbf34294de17b0d0e54aabbf2ef1
Yura Nesterovych <yranest2001@gmail.com>
2021-08-03 14:55:52 +0300

    Feature/#236

--- src/main/java/com/ss/ita/kata/implementation/yranest/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/yranest/EightImpl.java
@@ -20,7 +20,16 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        {
+            for(int i = 0; i<array.length;i++){
+                if ((Math.sqrt(array[i]) - Math.floor(Math.sqrt(array[i]))) == 0) {
+                    array[i] = (int)Math.sqrt(array[i]);
+                }
+                else{
+                    array[i]*=array[i];
+                }}
+            return array;
+        }
     }
 
     @Override
