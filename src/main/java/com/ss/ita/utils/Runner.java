45ce4cc0dbc3459c74e7ce9008f4a167d2d02ecb
Ivanna <veselka19012001@gmail.com>
2021-08-06 01:43:22 +0300

    base of class Runner

---
+++ src/main/java/com/ss/ita/utils/Runner.java
@@ -0,0 +1,92 @@
+package com.ss.ita.utils;
+
+import com.ss.ita.kata.Eight;
+import com.ss.ita.kata.Five;
+import com.ss.ita.kata.Seven;
+import com.ss.ita.kata.Six;
+
+
+public class Runner {
+    private Eight eight;
+    private Seven seven;
+    private Six six;
+    private Five five;
+    private Users users;
+    //
+    // We must REFACTOR sc field
+    //
+    private Scanner sc;
+    //
+
+    public Runner(int user) {
+        setUsers(Users.getUserById(user));
+
+//        The next line should be uncommented and REFACTORED according to sc field
+
+//        sc = new Scanner(System.in);
+    }
+
+    public boolean setUsers(Users users) {
+        this.users = users;
+        switch (users) {
+            case IVANKA:
+                five = new com.ss.ita.kata.implementation.ivannaveselovska.FiveImpl();
+                six = new com.ss.ita.kata.implementation.ivannaveselovska.SixImpl();
+                seven = new com.ss.ita.kata.implementation.ivannaveselovska.SevenImpl();
+                eight = new com.ss.ita.kata.implementation.ivannaveselovska.EightImpl();
+                return true;
+//                break;
+            case VOLODYMYR_MUR:
+                five = new com.ss.ita.kata.implementation.vmuravskyi.FiveImpl();
+                six = new com.ss.ita.kata.implementation.vmuravskyi.SixImpl();
+                seven = new com.ss.ita.kata.implementation.vmuravskyi.SevenImpl();
+                eight = new com.ss.ita.kata.implementation.vmuravskyi.EightImpl();
+                return true;
+//                break;
+            case Yurii:
+                five = new com.ss.ita.kata.implementation.yranest.FiveImpl();
+                six = new com.ss.ita.kata.implementation.yranest.SixImpl();
+                seven = new com.ss.ita.kata.implementation.yranest.SevenImpl();
+                eight = new com.ss.ita.kata.implementation.yranest.EightImpl();
+                return true;
+//                break;
+            case MAKSYM_DEN:
+                five = new com.ss.ita.kata.implementation.maxde1.FiveImpl();
+                six = new com.ss.ita.kata.implementation.maxde1.SixImpl();
+                seven = new com.ss.ita.kata.implementation.maxde1.SevenImpl();
+                eight = new com.ss.ita.kata.implementation.maxde1.EightImpl();
+                return true;
+//                break;
+            case ANASTASIIA:
+                five = new com.ss.ita.kata.implementation.kubrub.FiveImpl();
+                six = new com.ss.ita.kata.implementation.kubrub.SixImpl();
+                seven = new com.ss.ita.kata.implementation.kubrub.SevenImpl();
+                eight = new com.ss.ita.kata.implementation.kubrub.EightImpl();
+                return true;
+//                break;
+            case IEVGENII:
+                five = new com.ss.ita.kata.implementation.shcherbakoveugene.FiveImpl();
+                six = new com.ss.ita.kata.implementation.shcherbakoveugene.SixImpl();
+                seven = new com.ss.ita.kata.implementation.shcherbakoveugene.SevenImpl();
+                eight = new com.ss.ita.kata.implementation.shcherbakoveugene.EightImpl();
+                return true;
+//                break;
+            case VOLODYMYR_SHO:
+                five = new com.ss.ita.kata.implementation.vovik4.FiveImpl();
+                six = new com.ss.ita.kata.implementation.vovik4.SixImpl();
+                seven = new com.ss.ita.kata.implementation.vovik4.SevenImpl();
+                eight = new com.ss.ita.kata.implementation.vovik4.EightImpl();
+                return true;
+//                break;
+            case VIKTOR:
+//                five = new com.ss.ita.kata.implementation..FiveImpl();
+//                six = new com.ss.ita.kata.implementation..SixImpl();
+//                seven = new com.ss.ita.kata.implementation..SevenImpl();
+//                eight = new com.ss.ita.kata.implementation..EightImpl();
+                return true;
+//                break;
+        }
+        return false;
+    }
+
+}
