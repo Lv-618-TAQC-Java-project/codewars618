e03c980c9caaad64e5672040a75a04fc1da82518
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-08-06 19:23:41 +0300

    Artificial Rain

--- src/main/java/com/ss/ita/kata/implementation/vmuravskyi/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vmuravskyi/FiveImpl.java
@@ -7,7 +7,32 @@ import java.math.BigInteger;
 public class FiveImpl implements Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+        if (v.length == 0) {
+            return 0;
+        } else if (v.length == 1) {
+            return 1;
+        }
+
+        int result = 0;
+        int[] left = new int[v.length];
+        int[] right = new int[v.length];
+
+        for (int i = 1; i < v.length; i++)
+            if (v[i - 1] <= v[i]) {
+                left[i] = left[i - 1] + 1;
+            }
+
+        for (int i = v.length - 2; i >= 0; i--)
+            if (v[i + 1] <= v[i]) {
+                right[i] = right[i + 1] + 1;
+            }
+
+        for (int i = 0; i < v.length; i++) {
+            int currentSize = left[i] + right[i] + 1;
+            if (currentSize > result)
+                result = currentSize;
+        }
+        return result;
     }
 
     @Override
