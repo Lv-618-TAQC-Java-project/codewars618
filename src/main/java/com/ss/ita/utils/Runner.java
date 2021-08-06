dbdfa47729178e689667439a448f6aa1b6fea0f4
Ivanna <veselka19012001@gmail.com>
2021-08-06 23:26:53 +0300

    setter and constructor fixed

--- src/main/java/com/ss/ita/utils/Runner.java
+++ src/main/java/com/ss/ita/utils/Runner.java
@@ -12,21 +12,14 @@ public class Runner {
     private Six six;
     private Five five;
     private Users users;
-    //
-    // We must REFACTOR sc field
-    //
-    private Scanner sc;
-    //
+    private ConsoleScanner sc;
 
-    public Runner(int user) {
-        setUsers(Users.getUserById(user));
-
-//        The next line should be uncommented and REFACTORED according to sc field
-
-//        sc = new Scanner(System.in);
+    public Runner(Users user) {
+        setUsers(user);
+        sc = new ConsoleScanner();
     }
 
-    public boolean setUsers(Users users) {
+    public void setUsers(Users users) {
         this.users = users;
         switch (users) {
             case IVANKA:
@@ -34,59 +27,51 @@ public class Runner {
                 six = new com.ss.ita.kata.implementation.ivannaveselovska.SixImpl();
                 seven = new com.ss.ita.kata.implementation.ivannaveselovska.SevenImpl();
                 eight = new com.ss.ita.kata.implementation.ivannaveselovska.EightImpl();
-                return true;
-//                break;
+                break;
             case VOLODYMYR_MUR:
                 five = new com.ss.ita.kata.implementation.vmuravskyi.FiveImpl();
                 six = new com.ss.ita.kata.implementation.vmuravskyi.SixImpl();
                 seven = new com.ss.ita.kata.implementation.vmuravskyi.SevenImpl();
                 eight = new com.ss.ita.kata.implementation.vmuravskyi.EightImpl();
-                return true;
-//                break;
+                break;
             case Yurii:
                 five = new com.ss.ita.kata.implementation.yranest.FiveImpl();
                 six = new com.ss.ita.kata.implementation.yranest.SixImpl();
                 seven = new com.ss.ita.kata.implementation.yranest.SevenImpl();
                 eight = new com.ss.ita.kata.implementation.yranest.EightImpl();
-                return true;
-//                break;
+                break;
             case MAKSYM_DEN:
                 five = new com.ss.ita.kata.implementation.maxde1.FiveImpl();
                 six = new com.ss.ita.kata.implementation.maxde1.SixImpl();
                 seven = new com.ss.ita.kata.implementation.maxde1.SevenImpl();
                 eight = new com.ss.ita.kata.implementation.maxde1.EightImpl();
-                return true;
-//                break;
+                break;
             case ANASTASIIA:
                 five = new com.ss.ita.kata.implementation.kubrub.FiveImpl();
                 six = new com.ss.ita.kata.implementation.kubrub.SixImpl();
                 seven = new com.ss.ita.kata.implementation.kubrub.SevenImpl();
                 eight = new com.ss.ita.kata.implementation.kubrub.EightImpl();
-                return true;
-//                break;
+                break;
             case IEVGENII:
                 five = new com.ss.ita.kata.implementation.shcherbakoveugene.FiveImpl();
                 six = new com.ss.ita.kata.implementation.shcherbakoveugene.SixImpl();
                 seven = new com.ss.ita.kata.implementation.shcherbakoveugene.SevenImpl();
                 eight = new com.ss.ita.kata.implementation.shcherbakoveugene.EightImpl();
-                return true;
-//                break;
+                break;
             case VOLODYMYR_SHO:
                 five = new com.ss.ita.kata.implementation.vovik4.FiveImpl();
                 six = new com.ss.ita.kata.implementation.vovik4.SixImpl();
                 seven = new com.ss.ita.kata.implementation.vovik4.SevenImpl();
                 eight = new com.ss.ita.kata.implementation.vovik4.EightImpl();
-                return true;
-//                break;
+                break;
             case VIKTOR:
 //                five = new com.ss.ita.kata.implementation..FiveImpl();
 //                six = new com.ss.ita.kata.implementation..SixImpl();
 //                seven = new com.ss.ita.kata.implementation..SevenImpl();
 //                eight = new com.ss.ita.kata.implementation..EightImpl();
-                return true;
-//                break;
+                break;
         }
-        return false;
     }
+
 
 }
