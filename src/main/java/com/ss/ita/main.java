5fc8901ab0f4b754b433e9be09f7ed39fe0238ce
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-08-06 23:19:49 +0300

    2.1

--- src/main/java/com/ss/ita/main.java
+++ src/main/java/com/ss/ita/main.java
@@ -1,7 +1,13 @@
 package com.ss.ita;
 
+import com.ss.ita.kata.implementation.vmuravskyi.FiveImpl;
+
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        FiveImpl f = new FiveImpl();
+        System.out.println(Arrays.toString(f.gap(8, 300, 400)));
     }
 }
