767fc04bdaa1b6202b5867a0692a2bf8c993ab8d
vmuravskyi <78860189+vmuravskyi@users.noreply.github.com>
2021-08-06 23:21:18 +0300

    clean2

--- src/main/java/com/ss/ita/main.java
+++ src/main/java/com/ss/ita/main.java
@@ -1,7 +1,6 @@
 package com.ss.ita;
 
-import com.ss.ita.kata.implementation.ivannaveselovska.SevenImpl;
-import com.ss.ita.kata.implementation.ivannaveselovska.SixImpl;
+
 
 public class main {
     public static void main(String[] args) {
