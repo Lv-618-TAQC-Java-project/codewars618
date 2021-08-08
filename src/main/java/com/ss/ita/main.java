2a6136dbbf754fb0c5e7ff2caedc1dc00f2d7743
Volodymyr Muravskyi <v.muravskyi@gmail.com>
2021-08-08 11:20:08 +0300

    clear main

--- src/main/java/com/ss/ita/main.java
+++ src/main/java/com/ss/ita/main.java
@@ -1,13 +1,7 @@
 package com.ss.ita;
 
-import com.ss.ita.kata.implementation.vmuravskyi.FiveImpl;
-
-import java.util.Arrays;
-
 public class main {
     public static void main(String[] args) {
 
-        FiveImpl f = new FiveImpl();
-        System.out.println(Arrays.toString(f.gap(8, 300, 400)));
     }
 }
