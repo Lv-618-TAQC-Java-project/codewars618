925aae3324c68108cf78c2c9e5b6cf49246682d9
Volodymyr Muravskyi <v.muravskyi@gmail.com>
2021-08-09 18:46:51 +0300

    initial commit test

--- src/test/java/com/ss/ita/kata/EightTest.java
+++ src/test/java/com/ss/ita/kata/EightTest.java
@@ -1,4 +1,22 @@
+package com.ss.ita.kata;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class EightTest {
-    
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetVolumeOfCuboid() {
+    }
 }
--- src/test/java/com/ss/ita/utils/ConsoleScannerTest.java
+++ src/test/java/com/ss/ita/utils/ConsoleScannerTest.java
@@ -1,4 +1,22 @@
+package com.ss.ita.utils;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class ConsoleScannerTest {
-    
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testReadDoubleArray() {
+    }
 }
