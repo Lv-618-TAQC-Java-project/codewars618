02ab38ada161acc11bfa4a84044551df472364db
Yura Nesterovych <yranest2001@gmail.com>
2021-08-11 20:49:48 +0300

    Test for Help the bookseller/#394

--- src/test/java/com/ss/ita/kata/SixDataProvider.java
+++ src/test/java/com/ss/ita/kata/SixDataProvider.java
@@ -1,4 +1,25 @@
 package com.ss.ita.kata;
 
+import org.testng.annotations.DataProvider;
+
 public class SixDataProvider extends DataProviderClass{
+
+    @DataProvider(name = "validHelpBooksellerDataProvider")
+    public static Object[][] validHelpBooksellerDataProvider(){
+        Object[][] testData = new Object[][]{
+                {new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"},new String[] {"A", "B"},"(A : 200) - (B : 1140)"},
+                {new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"},new String[] {""},""},
+                {new String[]{""},new String[] {"A", "B"},""},
+        };
+        return combine(sixImplPackageProvider(), testData);
+    }
+    @DataProvider(name = "invalidHelpBooksellerDataProvider")
+    public static Object[][] invalidHelpBooksellerDataProvider(){
+        Object[][] testData = new Object[][]{
+                {new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"},new String[] {"A", "B"},"(L : 3200) - (K : 1140)"},
+                {new String[]{"FWD 234", "WAD 321", "WDA 100", "BTSQ 890", "YWD 214"},new String[] {"A", "B"},"(A : 200) - (B : 1140)"},
+        };
+        return combine(sixImplPackageProvider(), testData);
+    }
+
 }
--- src/test/java/com/ss/ita/kata/SixTest.java
+++ src/test/java/com/ss/ita/kata/SixTest.java
@@ -1,10 +1,11 @@
 package com.ss.ita.kata;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
-public class SixTest {
+public class SixTest extends SixDataProvider{
 
     @Test
     public void testFindNb() {
@@ -30,7 +31,12 @@ public class SixTest {
     public void testNbaCup() {
     }
 
-    @Test
-    public void testStockSummary() {
+    @Test(dataProvider = "validHelpBooksellerDataProvider")
+    public void testStockSummary(Six imp,String[] lstOfArt, String[] lstOf1stLetter,String expected) {
+        Assert.assertEquals(imp.stockSummary(lstOfArt, lstOf1stLetter),expected);
+    }
+    @Test(dataProvider = "invalidHelpBooksellerDataProvider")
+    public void testStockSummaryInvalid(Six imp,String[] lstOfArt, String[] lstOf1stLetter,String expected) {
+        Assert.assertNotEquals(imp.stockSummary(lstOfArt, lstOf1stLetter),expected);
     }
 }
