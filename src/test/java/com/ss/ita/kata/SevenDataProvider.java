75afc893cb1a06af1c6847740d603360cd98879f
Max <max.mageriv@ukr.net>
2021-08-11 18:45:23 +0300

    Where is Vasya Tests added

--- src/test/java/com/ss/ita/kata/SevenDataProvider.java
+++ src/test/java/com/ss/ita/kata/SevenDataProvider.java
@@ -1,4 +1,24 @@
 package com.ss.ita.kata;
 
+import org.testng.annotations.DataProvider;
+
 public class SevenDataProvider extends DataProviderClass{
+    @DataProvider(name = "positiveWhereIsVasya")
+    public static Object[][] positiveWhereIsVasya(){
+        Object[][] testData = new Object[][]{
+                {3, 1, 1, 2},
+                {5, 2, 3, 3},
+                {6, 2, 3, 4}
+        };
+        return combine(sevenImplPackageProvider(), testData);
+    }
+    @DataProvider(name = "negativeWhereIsVasya")
+    public static Object[][] negativeWhereIsVasya(){
+        Object[][] testData = new Object[][]{
+                {-5, -1, 0, 0},
+                {2, 5, 3, 0},
+                {-111, 50, 50, 0}
+        };
+        return combine(sevenImplPackageProvider(), testData);
+    }
 }
--- src/test/java/com/ss/ita/kata/SevenTest.java
+++ src/test/java/com/ss/ita/kata/SevenTest.java
@@ -1,10 +1,11 @@
 package com.ss.ita.kata;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
-public class SevenTest {
+public class SevenTest extends SevenDataProvider{
 
     @Test
     public void testNewAvg() {
@@ -14,7 +15,13 @@ public class SevenTest {
     public void testSeriesSum() {
     }
 
-    @Test
-    public void testWhereIsHe() {
+    @Test(dataProvider = "positiveWhereIsVasya")
+    public void positiveTestWhereIsHe(Seven imp, int a, int b, int c, int expected) {
+        Assert.assertEquals(imp.whereIsHe(a, b, c), expected);
+    }
+
+    @Test(dataProvider = "negativeWhereIsVasya")
+    public void negativeWhereIsVasya(Seven imp, int a, int b, int c, int expected){
+        Assert.assertEquals(imp.whereIsHe(a, b, c), expected);
     }
 }
