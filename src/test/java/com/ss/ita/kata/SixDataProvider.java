12da1f55e2294369850074c0c282a88562ade38b
Yura Nesterovych <yranest2001@gmail.com>
2021-08-12 16:38:59 +0300

    Test for Help the bookseller/#394

--- src/test/java/com/ss/ita/kata/SixDataProvider.java
+++ src/test/java/com/ss/ita/kata/SixDataProvider.java
@@ -8,8 +8,7 @@ public class SixDataProvider extends DataProviderClass{
     public static Object[][] validHelpBooksellerDataProvider(){
         Object[][] testData = new Object[][]{
                 {new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"},new String[] {"A", "B"},"(A : 200) - (B : 1140)"},
-                {new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"},new String[] {""},""},
-                {new String[]{""},new String[] {"A", "B"},""},
+
         };
         return combine(sixImplPackageProvider(), testData);
     }
