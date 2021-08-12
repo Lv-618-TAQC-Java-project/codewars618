5c47411e02cc5e4e242c6b803bf15e254f291446
Volodymyr Muravskyi <v.muravskyi@gmail.com>
2021-08-12 23:12:12 +0300

    task Zeros for testing

--- src/main/java/com/ss/ita/kata/implementation/vmuravskyi/FiveImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/vmuravskyi/FiveImpl.java
@@ -17,7 +17,26 @@ public class FiveImpl implements Five {
 
     @Override
     public int zeros(int n) {
-        return 0;
+        BigInteger sum = new BigInteger(String.valueOf(1));
+        BigInteger multiply = new BigInteger(String.valueOf(1));
+        BigInteger one = new BigInteger(String.valueOf(1));
+        for (int i = 1; i <= n; i++) {
+            sum = sum.multiply(multiply);
+            multiply = multiply.add(one);
+        }
+
+        String str = String.valueOf(sum);
+        int length = str.length();
+        int result = 0;
+        for (int i = 0; i < length; i++) {
+            String temp = String.valueOf(str.charAt(length - 1));
+            if (temp.equals("0")) {
+                result++;
+                length--;
+            }
+        }
+        return result;
+
     }
 
     @Override
