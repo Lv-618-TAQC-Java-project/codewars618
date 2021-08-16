0cafb3759e9dc15d5cb6caac9afcec58221eb5f5
Ivanna <veselka19012001@gmail.com>
2021-08-17 00:37:28 +0300

    tests for squareOrSquareRoot function is implemented

--- src/test/java/com/ss/ita/kata/EightDataProvider.java
+++ src/test/java/com/ss/ita/kata/EightDataProvider.java
@@ -110,4 +110,24 @@ public class EightDataProvider extends DataProviderClass {
         };
         return combine(eightImplPackageProvider(), testData);
     }
+
+    @DataProvider()
+    public static Object[][] positiveSquareOrSquareRoot(){
+        Object[][] testData = new Object[][]{
+                {new int[]{4, 3, 9, 7, 2, 1}, new int[]{2, 9, 3, 49, 4, 1}},
+                {new int[]{100, 101, 5, 5, 1, 1}, new int[]{10, 10201, 25, 25, 1, 1}},
+                {new int[]{1, 2, 3, 4, 5, 6}, new int[]{1, 4, 9, 2, 25, 36}}
+        };
+        return combine(eightImplPackageProvider(), testData);
+    }
+
+    @DataProvider()
+    public static Object[][] negativeSquareOrSquareRoot(){
+        Object[][] testData = new Object[][]{
+                {new int[]{-1, -2, -3, -4, -5, -6}, new int[0]},
+                {new int[0], new int[0]},
+                {new int[]{1, 2, 3, 4, 0, 6}, new int[0]}
+        };
+        return combine(eightImplPackageProvider(), testData);
+    }
 }
