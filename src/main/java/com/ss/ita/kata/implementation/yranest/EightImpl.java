d6e08f6ebd19f7b2bead8a550c2f2b729ed0da1a
Yura Nesterovych <yranest2001@gmail.com>
2021-08-17 17:05:50 +0300

    Feature/#472 fix some mistakes in tasks

--- src/main/java/com/ss/ita/kata/implementation/yranest/EightImpl.java
+++ src/main/java/com/ss/ita/kata/implementation/yranest/EightImpl.java
@@ -10,7 +10,11 @@ public class EightImpl implements Eight {
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return  length*width*height;
+        if (length <= 0 || width <= 0 || height <= 0) {
+            return -1;
+        }
+        else
+            return  length*width*height;
     }
 
     @Override
