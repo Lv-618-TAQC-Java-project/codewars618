c4a0d6800ead4413c4b2ab7abcafd2c56d9b1f0a
Maxde1 <maxdeniss56@gmail.com>
2021-08-17 08:56:14 +0300

    Fix DataProvider

--- src/test/java/com/ss/ita/kata/ConsoleScannerDataProvider.java
+++ src/test/java/com/ss/ita/kata/ConsoleScannerDataProvider.java
@@ -21,4 +21,29 @@ public class ConsoleScannerDataProvider extends DataProviderClass {
         };
         return combine(ConsoleScannerProvider(), param);
     }
+    @DataProvider(name = "positiveReadIntArray")
+    public static Object[][] positiveReadIntArray(){
+        Object[][] testData = new Object[][]{
+                {"1 3 5 6 6", new int[]{1, 3, 5, 6, 6}},
+                {"1 4343 2534 0000", new int[]{1, 4343, 2534, 0}},
+                {"0", new int[]{0}},
+                {" ", new int[]{}},
+                {"0001 000002 000023", new int[]{1, 2, 23}}
+        };
+        return testData;
+    }
+    @DataProvider(name = "negativeReadIntArray")
+    public static Object[][] negativeReadIntArray(){
+        Object[][] testData = new Object[][]{
+                {"1  1  2  4  5"},
+                {"re1 433 t4353"},
+                {"# 12% 0"},
+                {"343_242_42_943"},
+                {"1, 3, 5, 6"},
+                {"1111111111111111111111111111111111222222222222"},
+                {"\"1 2 4 5 6\""},
+                {"\"1\", \"2\""}
+        };
+        return testData;
+    }
 }
--- src/test/java/com/ss/ita/utils/ConsoleScannerTest.java
+++ src/test/java/com/ss/ita/utils/ConsoleScannerTest.java
@@ -4,10 +4,29 @@ import com.ss.ita.kata.ConsoleScannerDataProvider;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class ConsoleScannerTest extends ConsoleScannerDataProvider {
 
     @Test(dataProvider = "validConsoleScanner")
     public void validTestReadDoubleArray(ConsoleScanner scanner, String[] arr, String expected) {
 //        Assert.assertEquals(scanner.readDoubleArray(arr), expected);
     }
+    @Test(dataProvider = "positiveReadIntArray")
+    public void positiveTestReadIntArray(String input, int[] result){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        ConsoleScanner sc = new ConsoleScanner();
+        Assert.assertEquals(result,sc.readIntArray());
+    }
+    @Test(dataProvider = "negativeReadIntArray", expectedExceptions = Exception.class)
+    public void negativeReadIntArray(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        ConsoleScanner sc = new ConsoleScanner();
+        sc.readIntArray();
+    }
+
+
 }
