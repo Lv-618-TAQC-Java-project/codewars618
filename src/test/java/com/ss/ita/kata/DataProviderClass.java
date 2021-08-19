8e0a183ff718a74684863f9ae0d6268654b89cba
Ivanna <veselka19012001@gmail.com>
2021-08-19 14:16:19 +0300

    implementations uncommented

--- src/test/java/com/ss/ita/kata/DataProviderClass.java
+++ src/test/java/com/ss/ita/kata/DataProviderClass.java
@@ -2,8 +2,6 @@ package com.ss.ita.kata;
 
 import org.testng.annotations.DataProvider;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,28 +10,28 @@ public class DataProviderClass {
     @DataProvider(name = "eightImplPackageProvider")
     public static Object[][] eightImplPackageProvider() {
         return new Object[][]{
-                {new com.ss.ita.kata.implementation.ivannaveselovska.EightImpl()}//,
-//                {new com.ss.ita.kata.implementation.kubrub.EightImpl()},
-//                {new com.ss.ita.kata.implementation.maxde1.EightImpl()},
-//                {new com.ss.ita.kata.implementation.msemochko.EightImpl()},
-//                {new com.ss.ita.kata.implementation.shcherbakoveugene.EightImpl()},
-//                {new com.ss.ita.kata.implementation.vmuravskyi.EightImpl()},
-//                {new com.ss.ita.kata.implementation.vovik4.EightImpl()},
-//                {new com.ss.ita.kata.implementation.yranest.EightImpl()}
+                {new com.ss.ita.kata.implementation.ivannaveselovska.EightImpl()},
+                {new com.ss.ita.kata.implementation.kubrub.EightImpl()},
+                {new com.ss.ita.kata.implementation.maxde1.EightImpl()},
+                {new com.ss.ita.kata.implementation.msemochko.EightImpl()},
+                {new com.ss.ita.kata.implementation.shcherbakoveugene.EightImpl()},
+                {new com.ss.ita.kata.implementation.vmuravskyi.EightImpl()},
+                {new com.ss.ita.kata.implementation.vovik4.EightImpl()},
+                {new com.ss.ita.kata.implementation.yranest.EightImpl()}
         };
     }
 
     @DataProvider(name = "sevenImplPackageProvider")
     public static Object[][] sevenImplPackageProvider() {
         return new Object[][]{
-                {new com.ss.ita.kata.implementation.ivannaveselovska.SevenImpl()}//,
-//                {new com.ss.ita.kata.implementation.kubrub.SevenImpl()},
-//                {new com.ss.ita.kata.implementation.maxde1.SevenImpl()},
-//                {new com.ss.ita.kata.implementation.msemochko.SevenImpl()},
-//                {new com.ss.ita.kata.implementation.shcherbakoveugene.SevenImpl()},
-//                {new com.ss.ita.kata.implementation.vmuravskyi.SevenImpl()},
-//                {new com.ss.ita.kata.implementation.vovik4.SevenImpl()},
-//                {new com.ss.ita.kata.implementation.yranest.SevenImpl()}
+                {new com.ss.ita.kata.implementation.ivannaveselovska.SevenImpl()},
+                {new com.ss.ita.kata.implementation.kubrub.SevenImpl()},
+                {new com.ss.ita.kata.implementation.maxde1.SevenImpl()},
+                {new com.ss.ita.kata.implementation.msemochko.SevenImpl()},
+                {new com.ss.ita.kata.implementation.shcherbakoveugene.SevenImpl()},
+                {new com.ss.ita.kata.implementation.vmuravskyi.SevenImpl()},
+                {new com.ss.ita.kata.implementation.vovik4.SevenImpl()},
+                {new com.ss.ita.kata.implementation.yranest.SevenImpl()}
         };
     }
 
@@ -41,27 +39,27 @@ public class DataProviderClass {
     public static Object[][] sixImplPackageProvider() {
         return new Object[][]{
                 {new com.ss.ita.kata.implementation.ivannaveselovska.SixImpl()}//,
-//                {new com.ss.ita.kata.implementation.kubrub.SixImpl()},
-//                {new com.ss.ita.kata.implementation.maxde1.SixImpl()},
-//                {new com.ss.ita.kata.implementation.msemochko.SixImpl()},
-//                {new com.ss.ita.kata.implementation.shcherbakoveugene.SixImpl()},
-//                {new com.ss.ita.kata.implementation.vmuravskyi.SixImpl()},
-//                {new com.ss.ita.kata.implementation.vovik4.SixImpl()},
-//                {new com.ss.ita.kata.implementation.yranest.SixImpl()}
+                {new com.ss.ita.kata.implementation.kubrub.SixImpl()},
+                {new com.ss.ita.kata.implementation.maxde1.SixImpl()},
+                {new com.ss.ita.kata.implementation.msemochko.SixImpl()},
+                {new com.ss.ita.kata.implementation.shcherbakoveugene.SixImpl()},
+                {new com.ss.ita.kata.implementation.vmuravskyi.SixImpl()},
+                {new com.ss.ita.kata.implementation.vovik4.SixImpl()},
+                {new com.ss.ita.kata.implementation.yranest.SixImpl()}
         };
     }
 
     @DataProvider(name = "fiveImplPackageProvider")
     public static Object[][] fiveImplPackageProvider() {
         return new Object[][]{
-                {new com.ss.ita.kata.implementation.ivannaveselovska.FiveImpl()}//,
-//                {new com.ss.ita.kata.implementation.kubrub.FiveImpl()},
-//                {new com.ss.ita.kata.implementation.maxde1.FiveImpl()},
-//                {new com.ss.ita.kata.implementation.msemochko.FiveImpl()},
-//                {new com.ss.ita.kata.implementation.shcherbakoveugene.FiveImpl()},
-//                {new com.ss.ita.kata.implementation.vmuravskyi.FiveImpl()},
-//                {new com.ss.ita.kata.implementation.vovik4.FiveImpl()},
-//                {new com.ss.ita.kata.implementation.yranest.FiveImpl()}
+                {new com.ss.ita.kata.implementation.ivannaveselovska.FiveImpl()},
+                {new com.ss.ita.kata.implementation.kubrub.FiveImpl()},
+                {new com.ss.ita.kata.implementation.maxde1.FiveImpl()},
+                {new com.ss.ita.kata.implementation.msemochko.FiveImpl()},
+                {new com.ss.ita.kata.implementation.shcherbakoveugene.FiveImpl()},
+                {new com.ss.ita.kata.implementation.vmuravskyi.FiveImpl()},
+                {new com.ss.ita.kata.implementation.vovik4.FiveImpl()},
+                {new com.ss.ita.kata.implementation.yranest.FiveImpl()}
         };
     }
 
