7c5588778527ab782ff37e1690ad339ae08dd337
Volodymyr Muravskyi <v.muravskyi@gmail.com>
2021-09-09 22:53:22 +0300

    add surefire for jenkins

--- src/main/java/com/ss/ita/main.java
+++ src/main/java/com/ss/ita/main.java
@@ -1,9 +1,7 @@
 package com.ss.ita;
 
-import com.ss.ita.kata.implementation.vmuravskyi.EightImpl;
 import com.ss.ita.utils.Menu;
 
-import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
